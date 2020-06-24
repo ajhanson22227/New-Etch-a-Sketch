00cff0d6ae09836cf9dc0c39107b4b8dff98b078
adam <ajhanson22227@gmail.com>
2020-06-23 20:53:07 -0500

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let etchContainer = document.getElementById("etchContainer");
+let etchSquareDiv = document.createElement("div");
+etchSquareDiv.classList.add("etchSquare");
+etchSquareDiv.style.height= "100px";
+etchSquareDiv.style.width = "100px";
+
+etchContainer.appendChild(etchSquareDiv);
