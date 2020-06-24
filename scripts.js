5886b466ea66020316967b55a0ff967351770334
adam <ajhanson22227@gmail.com>
2020-06-23 21:40:53 -0500

    added grid creation functionality

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
 let etchContainer = document.getElementById("etchContainer");
-let etchSquareDiv = document.createElement("div");
-etchSquareDiv.classList.add("etchSquare");
-etchSquareDiv.style.height= "100px";
-etchSquareDiv.style.width = "100px";
+let etchSquareListener = document.getElementsByClassName('etchSquare')
 
-etchContainer.appendChild(etchSquareDiv);
+
+//gridSize will be the variable that will change depending on user input. On window load, it becomes 16
+let gridSize;
+
+
+
+
+
+
+
+//this will start with a 16x16 grid on first open
+function createGrid(totalGridSize){
+    gridSize = totalGridSize;
+
+    etchContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    etchContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    
+    for (let i = 0; i < gridSize * gridSize; i++){
+        let etchSquareDiv = document.createElement("div");
+        etchSquareDiv.className = 'etchSquare';
+        etchContainer.appendChild(etchSquareDiv);
+    }    
+}
